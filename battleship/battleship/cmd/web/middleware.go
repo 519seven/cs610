7234b27475ecd8111cf5c1bfdeb1661cc2b51df2
519Seven <519seven@users.noreply.github.com>
2019-09-22 13:15:22 -0400

    adding a battleship that works on go 1.13

---
+++ battleship/battleship/cmd/web/middleware.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// add headers to help prevent XSS and Clickjacking attacks
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Frame-Options", "deny")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// recover when the stack unwinds
+		defer func() {
+			if err := recover(); err != nil {
+				// set a connection close header on the response
+				w.Header().Set("Connection", "close")
+				// call app.serverError to return a 500 response
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
