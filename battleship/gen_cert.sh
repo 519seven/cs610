2a90ae607fe47d31d206696b35ce5de9f6b2e267
519seven <pjakey@gmail.com>
2019-10-18 12:14:05 +0000

    scripts for a fresh machine

--- battleship/gen_cert.sh
+++ battleship/gen_cert.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+[[ -f .install_env ]] && { printf "Sourcing .install_env\n"; . .install_env; } || { printf ".install_env not found. Exiting.\n"; exit -1; }
+
+GOVERSION=$go_version
+GOPROGRAM=$go_program
 mkdir -p tls
 cd tls
 printf "Finding your home directory\n"
 homedir=$(getent passwd "$USER" | cut -d: -f6)
 printf "Using $homedir as your home directory\n"
 printf "Searching for generate_cert.go (this may take a while)\n"
-gencert=$(find $homedir -type d -name go1.13 -exec find {} -name generate_cert.go -print \;)
-printf "Generating RSA key pair...\n"
-go1.13 run $gencert --rsa-bits=2048 --host=localhost
+gencert=$(find $homedir -type d -name '*go*' -exec find {} -name generate_cert.go -print \;)
+printf "Generating RSA key pair using $GOPROGRAM and $gencert...\n"
+$GOPROGRAM run $gencert --rsa-bits=2048 --host=localhost
 printf "Storing private key in key.pem file\n"
 printf "Generating self-signed TLS certificate for localhost and storing in cert.pem file\n"
